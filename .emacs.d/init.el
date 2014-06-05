8c55978b870781060b449f32e8efd3928db13ce5
Ben Pedrick <ben.pedrick@gmail.com>
2014-06-05 09:42:08 -0700

    FIX java indentation in emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -100,13 +100,16 @@
 (ido-mode 1)
 
 ; Java mode
+(defun my-java-indent-setup ()
+        (c-set-offset 'arglist-intro '+))
 (add-hook 'java-mode-hook
           (lambda ()
             (setq c-basic-offset 4
                   tab-width 4
                   indent-tabs-mode t)
             (setq whitespace-display-mappings '())
-            (outline-minor-mode 1)))
+            (my-java-indent-setup)
+            ))
 
 ; Makefile mode
 (add-hook 'makefile-mode-hook
