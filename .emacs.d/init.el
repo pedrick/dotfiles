38d49a933a640430969fafe01744e1007842be9d
Ben Pedrick <ben.pedrick@gmail.com>
2014-08-28 10:06:42 -0700

    FIX use autoload as possible for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,16 +25,18 @@
 (load-theme 'solarized-light t)
 (menu-bar-mode 0)
 
-(require 'whitespace)
-(setq whitespace-line-column 80
-      whitespace-style '(face trailing lines-tail space-mark tab-mark
-                              newline-mark))
-(setq whitespace-display-mappings
-       ;; all numbers are Unicode codepoint in decimal.
-      '(
-        (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
-        ))
+(autoload 'whitespace "whitespace" "Whitespace mode" t)
 (global-whitespace-mode 1)
+(eval-after-load "whitespace"
+  '(progn
+     (setq whitespace-line-column 80
+           whitespace-style '(face trailing lines-tail space-mark tab-mark
+                                   newline-mark))
+     (setq whitespace-display-mappings
+           ;; all numbers are Unicode codepoint in decimal.
+           '(
+             (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+             ))))
 
 ;;; Backup Directory
 (setq
@@ -62,82 +64,82 @@
 
 
 ; Cython mode
-(require 'cython-mode)
+(autoload 'cython-mode "cython-mode" "Cython mode" t)
 
 ; Ediff
-(require 'ediff)
-(setq ediff-split-window-function 'split-window-horizontally)
+(autoload 'ediff "ediff" "Ediff mode" t)
+(eval-after-load "ediff"
+  (setq ediff-split-window-function 'split-window-horizontally))
 
 ; Evil mode
-(setq evil-want-C-i-jump nil)  ; Don't bind <TAB>
-(require 'evil)
+(autoload 'evil "evil" "Evil mode" t)
 (evil-mode 1)
+(eval-after-load "evil"
+  '(progn
+     (setq evil-want-C-i-jump nil)  ; Don't bind <TAB>
+     (setq evil-auto-indent t)
+     (define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+     (define-key evil-insert-state-map [remap newline] 'evil-ret-and-indent)
 
-(setq evil-auto-indent t)
-(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
-(define-key evil-insert-state-map [remap newline] 'evil-ret-and-indent)
-
-(evil-define-command cofi/maybe-exit ()
-  :repeat change
-  (interactive)
-  (let ((modified (buffer-modified-p)))
-    (insert "j")
-    (let ((evt (read-event (format "Insert %c to exit insert state" ?k)
-                           nil 0.5)))
-      (cond
-       ((null evt) (message ""))
-       ((and (integerp evt) (char-equal evt ?k))
-        (delete-char -1)
-        (set-buffer-modified-p modified)
-        (push 'escape unread-command-events))
-       (t (setq unread-command-events (append unread-command-events
-                                              (list evt))))))))
+     (evil-define-command cofi/maybe-exit ()
+       :repeat change
+       (interactive)
+       (let ((modified (buffer-modified-p)))
+         (insert "j")
+         (let ((evt (read-event (format "Insert %c to exit insert state" ?k)
+                                nil 0.5)))
+           (cond
+            ((null evt) (message ""))
+            ((and (integerp evt) (char-equal evt ?k))
+             (delete-char -1)
+             (set-buffer-modified-p modified)
+             (push 'escape unread-command-events))
+            (t (setq unread-command-events (append unread-command-events
+                                                   (list evt))))))))))
 
 ; Flycheck
-(require 'flycheck)
+(autoload 'flycheck "flycheck" "Flycheck mode" t)
 (global-flycheck-mode 1)
-(setq-default flycheck-flake8-maximum-line-length 80)
-(add-hook 'flycheck-error-list-mode-hook
-          '(lambda () (evil-emacs-state 1)))
+(eval-after-load "flycheck"
+  '(progn
+     (setq-default flycheck-flake8-maximum-line-length 80)
+     (add-hook 'flycheck-error-list-mode-hook
+               '(lambda () (evil-emacs-state 1)))))
 
 ; Haskell mode
 (add-hook 'haskell-mode-hook
           'turn-on-haskell-indentation)
 
 ; Helm mode
-(require 'helm)
+(autoload 'helm "helm" "Helm mode" t)
 (helm-mode 1)
 
+(eval-after-load "helm"
+  '(progn
+     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+     (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+     (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-
-(setq helm-ff-skip-boring-files t)
-(cl-loop for ext in '("\\.pyc$")
-      do (add-to-list 'helm-boring-file-regexp-list "\\.pyc$"))
-
-; Ido mode
-;(require 'ido)
-;(setq ido-enable-flex-matching t)
-;(setq ido-everywhere t)
-;(ido-mode 1)
+     (setq helm-ff-skip-boring-files t)
+     (cl-loop for ext in '("\\.pyc$")
+              do (add-to-list 'helm-boring-file-regexp-list "\\.pyc$"))))
 
 ; Java mode
-(require 'cc-mode)
 (defun my-java-indent-setup ()
         (c-set-offset 'arglist-intro '+))
-(add-hook 'java-mode-hook
-          (lambda ()
-            (setq c-basic-offset 4
-                  tab-width 4
-                  indent-tabs-mode t)
-            (setq whitespace-display-mappings '())
-            (my-java-indent-setup)
-            ))
+(eval-after-load "cc-mode"
+  '(progn
+     (add-hook 'java-mode-hook
+               (lambda ()
+                 (setq c-basic-offset 4
+                       tab-width 4
+                       indent-tabs-mode t)
+                 (setq whitespace-display-mappings '())
+                 (my-java-indent-setup)
+                 ))))
 
 ; Magit mode
-(require 'magit)
+(autoload 'magit "magit" "Magit mode" t)
 (eval-after-load 'magit
   '(progn
      (evil-define-key 'normal git-rebase-mode-map
@@ -156,31 +158,37 @@
             (modify-syntax-entry ?= "'")))
 
 ; Org mode
-(require 'org)
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-(setq org-log-done 'time)
+(autoload 'org "org" "Org mode" t)
+(eval-after-load "org"
+  '(progn
+     (global-set-key "\C-cl" 'org-store-link)
+     (global-set-key "\C-cc" 'org-capture)
+     (global-set-key "\C-ca" 'org-agenda)
+     (global-set-key "\C-cb" 'org-iswitchb)
+     (setq org-log-done 'time)))
 
 ; Python mode
-(require 'python)
-(add-hook 'python-mode-hook
-          (lambda ()
-            (setq tab-width 4)
-            (setq python-indent-offset 4)))
+(autoload 'python "python" "Python mode" t)
+(eval-after-load "python"
+  '(add-hook 'python-mode-hook
+             (lambda ()
+               (setq tab-width 4)
+               (setq python-indent-offset 4))))
 
 ; Scss-mode
-(require 'scss-mode)
-(add-hook 'scss-mode-hook
-          (lambda ()
-            (setq scss-compile-at-save nil)))
+(autoload 'scss-mode "scss-mode" "Scss mode" t)
+(eval-after-load "scss-mode"
+  '(add-hook 'scss-mode-hook
+             (lambda ()
+               (setq scss-compile-at-save nil))))
 
 ; Undo-tree mode
-(require 'undo-tree)
+(autoload 'undo-tree "undo-tree" "Undo-Tree mode" t)
 (global-undo-tree-mode)
-(setq undo-tree-visualizer-timestamps t)
-(setq undo-tree-visualizer-diff t)
+(eval-after-load "undo-tree"
+  '(progn
+     (setq undo-tree-visualizer-timestamps t)
+     (setq undo-tree-visualizer-diff t)))
 
 ; Uniquify
 (require 'uniquify)
@@ -206,5 +214,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-selection ((t (:background "black" :underline t)))))
+ '(helm-selection ((t (:background "none" :underline t)))))
 ;;; init.el ends here
