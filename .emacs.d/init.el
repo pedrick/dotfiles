b89e6cb93191407d54b2130bf79798c363e623d9
Ben Pedrick <ben.pedrick@gmail.com>
2014-10-17 14:10:38 -0700

    ADD smart-mode-line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,10 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+
 (package-initialize)
 ;;; Load path
 (add-to-list 'load-path "~/.emacs.d/scripts")
@@ -186,6 +193,10 @@
              (lambda ()
                (setq scss-compile-at-save nil))))
 
+; Smart-mode-line
+(require 'smart-mode-line)
+(sml/setup)
+
 ; Undo-tree mode
 (autoload 'undo-tree "undo-tree" "Undo-Tree mode" t)
 (global-undo-tree-mode)
@@ -207,12 +218,6 @@
                          ("melpa"     . "http://melpa.milkbox.net/packages/")
                          ("tromey"    . "http://tromey.com/elpa/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
