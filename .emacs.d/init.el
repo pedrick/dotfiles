e8631f66b5cb3251364259b57b25ab1417707390
Ben Pedrick <ben.pedrick@gmail.com>
2014-10-24 16:40:46 -0700

    UPDATE emacs packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+ '(custom-safe-themes (quote ("59e21cf73dc1452801d0ff2b53cac27272569a0320a6081f5ee09f310683deb7" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
 
 (package-initialize)
 ;;; Load path
