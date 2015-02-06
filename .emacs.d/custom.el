e9671141edac09a7eb57b76ea9d2ac0226af29ed
Ben Pedrick <ben.pedrick@gmail.com>
2015-02-06 09:25:58 -0800

    FIX whitespace endline color

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "59e21cf73dc1452801d0ff2b53cac27272569a0320a6081f5ee09f310683deb7" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+ '(custom-safe-themes (quote ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "59e21cf73dc1452801d0ff2b53cac27272569a0320a6081f5ee09f310683deb7" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
+ '(global-whitespace-mode t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -12,5 +13,6 @@
  ;; If there is more than one, they won't work right.
  '(helm-selection ((t (:background "none" :underline t))))
  '(mode-line ((t (:background "color-251" :foreground "black" :inverse-video nil :box nil :underline nil :slant normal :weight normal))))
- '(mode-line-inactive ((t (:inherit mode-line :background "color-253" :foreground "grey20" :inverse-video nil :box nil :underline nil :slant normal :weight normal)))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "color-253" :foreground "grey20" :inverse-video nil :box nil :underline nil :slant normal :weight normal))))
+ '(whitespace-line ((t (:foreground "#af005f")))))
 ;;; custom.el ends here
