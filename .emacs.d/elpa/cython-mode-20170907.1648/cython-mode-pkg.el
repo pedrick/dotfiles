a5f3d91bebca872151e893317c201aedb187e6be
Ben Pedrick <ben.pedrick@gmail.com>
2018-06-26 10:44:28 -0700

    UPDATE emacs

--- .emacs.d/elpa/cython-mode-20170907.1648/cython-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "cython-mode" "20170907.1648" "Major mode for editing Cython files" 'nil :commit "cb511f978f8319d15a6552bcc6c0390123804b19")
---
+++ .emacs.d/elpa/cython-mode-20180213.854/cython-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cython-mode" "20180213.854" "Major mode for editing Cython files" 'nil :commit "084a25f55d0b4cf8b4c3cd496ec57bb3e2f57f71")
