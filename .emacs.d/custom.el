843e021feb7d4b222fc9623d5d66b0bd1ba05658
Ben Pedrick <ben.pedrick@gmail.com>
2020-11-17 20:59:02 -0500

    Add C# and Typescript emacs modes
    Useful for new job at Fastcase.

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,13 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "59e21cf73dc1452801d0ff2b53cac27272569a0320a6081f5ee09f310683deb7" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
+   '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "31a01668c84d03862a970c471edbd377b2430868eccf5e8a9aec6831f1a0908d" "1297a022df4228b81bc0436230f211bad168a117282c20ddcba2db8c6a200743" "59e21cf73dc1452801d0ff2b53cac27272569a0320a6081f5ee09f310683deb7" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))
  '(global-whitespace-mode t)
  '(menu-bar-mode nil)
  '(package-selected-packages
-   (quote
-    (flycheck use-package scss-mode yaml-mode helm yasnippet solarized-theme smart-mode-line rainbow-delimiters org-ac org magit less-css-mode jedi helm-projectile haskell-mode haml-mode go-mode gitignore-mode gitconfig-mode flx f evil dockerfile-mode cython-mode color-theme coffee-mode)))
+   '(csharp-mode typescript-mode flycheck use-package scss-mode yaml-mode helm yasnippet solarized-theme smart-mode-line rainbow-delimiters org-ac org magit less-css-mode jedi helm-projectile haskell-mode haml-mode go-mode gitignore-mode gitconfig-mode flx f evil dockerfile-mode cython-mode color-theme coffee-mode))
  '(tool-bar-mode nil))
 
 (custom-set-faces
