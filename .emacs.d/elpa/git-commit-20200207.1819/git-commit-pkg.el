ea6d51c6f93f81c5cf1bd24e8803793858e15948
Ben Pedrick <ben.pedrick@gmail.com>
2021-05-25 18:42:42 -0400

    Update emacs packages

--- .emacs.d/elpa/git-commit-20200207.1819/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20200207.1819" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "e8c7bd0329194bf09d3e8992e78efbfcd2769f19" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
--- .emacs.d/elpa/log4e-20200419.933/log4e-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "log4e" "20200419.933" "provide logging framework for elisp" 'nil :commit "2540cb8d8fbb4b039a9fe41dfd956817d7ac0337" :keywords '("log") :authors '(("Hiroaki Otsu" . "ootsuhiroaki@gmail.com")) :maintainer '("Hiroaki Otsu" . "ootsuhiroaki@gmail.com") :url "https://github.com/aki2o/log4e")
