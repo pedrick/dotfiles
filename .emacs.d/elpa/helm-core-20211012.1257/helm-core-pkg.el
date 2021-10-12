4b933b84d0404f4169c83d5672aea81a044385b3
Ben Pedrick <ben.pedrick@gmail.com>
2021-10-12 16:37:41 -0400

    Update emacs packages

---
+++ .emacs.d/elpa/helm-core-20211012.1257/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20211012.1257" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "b2bd8a2a938030c70e7da84bd49cadcc095ec2b9")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
