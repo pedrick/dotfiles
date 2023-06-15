1f2c1b6f3f567e77bda8534efe7a737fdbe9e44f
Ben Pedrick <ben.pedrick@gmail.com>
2023-06-14 20:58:28 -0400

    Update emacs packages

---
+++ .emacs.d/elpa/php-mode-20230521.1700/php-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "php-mode" "20230521.1700" "Major mode for editing PHP code"
+  '((emacs "26.1"))
+  :commit "249b31c9b542e7a22050c97fb26ccbf44150b199" :authors
+  '(("Eric James Michael Ritz"))
+  :maintainers
+  '(("USAMI Kenta" . "tadsan@zonu.me"))
+  :maintainer
+  '("USAMI Kenta" . "tadsan@zonu.me")
+  :keywords
+  '("languages" "php")
+  :url "https://github.com/emacs-php/php-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- .emacs.d/elpa/yaxception-20150105.1452/yaxception-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; yaxception-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("yaxception.el") (23401 7386 678596 57000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; yaxception-autoloads.el ends here
